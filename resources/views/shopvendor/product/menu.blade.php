553d0e9d879511b258fe23bd0820dd34d91a3450
SubashMA <subash.ma96@gmail.com>
2025-05-18 22:47:12 +0530

    Minor Change

--- resources/views/shopvendor/product/menu.blade.php
+++ resources/views/shopvendor/product/menu.blade.php
@@ -15,7 +15,7 @@
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
                                 <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
-                                <li class="breadcrumb-item active">Product</li>
+                                <li class="breadcrumb-item active">Products</li>
                             </ol>
                         </div>
 
